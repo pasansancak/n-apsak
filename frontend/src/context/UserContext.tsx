c279745343a6024a9af39b61c99d1b76f8ca4bc3
Paşan Sancak <pasansancak@Pasan-MacBook-Pro.local>
2025-06-05 10:50:46 +0300

    ProfileScreen, MainTabNavigator and Navigation. UserContext Structure

---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+export type User = {
+  full_name: string;
+  email: string;
+  profile_image: string;
+  cover_image?: string;
+  bio?: string;
+  instagram?: string;
+  linkedin?: string;
+};
+
+type UserContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+};
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) throw new Error("useUser must be used within UserProvider");
+  return context;
+};
